6bd807d4bad2d6ab2207e0e193d0d216272c7742
GabrielleMoreno <127845040+GabrielleMoreno@users.noreply.github.com>
2024-09-12 01:12:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let anoAtual= 2024;
+const nomeCompleto = "Gabrielle Moreno";
+let idade = 17;
+const cursoFaculdade= "Medicina";
+let cidadeAtual= "São José dos Pinhais"
+
+console.log(`ola eu sou a ${nomeCompleto} tenho ${idade} anos, moro em ${cidadeAtual} quero ano que vem estar cursando ${cursoFaculdade}`);
+
+anoAtual=2025;
+idade = 18;
+cidadeAtual= "São José dos Pinhais";
+
+console.log(`ola eu sou a ${nomeCompleto} tenho ${idade} anos, moro em ${cidadeAtual} e estou cursando esse ano ${cursoFaculdade}`);
